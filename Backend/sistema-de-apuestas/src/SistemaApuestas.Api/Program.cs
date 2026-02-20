c8b4dc6b3dd498ae93f3849844702f763fa00604
DieguilloFC <a20216760@pucp.edu.pe>
2026-02-19 20:11:45 -0500

    Configuracion de Api UI Swagger
    Agregado de dependencias, conexion a db y configuracion de conexion con entityframework

--- Backend/sistema-de-apuestas/src/SistemaApuestas.Api/Program.cs
+++ Backend/sistema-de-apuestas/src/SistemaApuestas.Api/Program.cs
@@ -1,41 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaApuestas.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Registro de Controllers
+builder.Services.AddControllers();
+
+// AGREGAR SERVICIOS DE SWAGGER 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configuracion de la conexión a la base de datos PostgreSQL usando Entity Framework Core
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configuración de CORS para permitir solicitudes desde el frontend React
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // El puerto de tu React
+              .AllowAnyHeader()  // Permite enviar JSON y tokens de autorización
+              .AllowAnyMethod(); // Permite GET, POST, PUT, DELETE
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.UseCors("ReactFrontend"); // Aplica la política de CORS
+app.MapControllers(); // Mapea los controladores de la API
+app.Run(); // Inicia la aplicación
